59c96802f970429cddd86e67adae4cbcc9115068
simonpascal <simon.pascal.ngos@accenture.com>
2019-04-22 13:25:47 +0400

    add angular

---
+++ src/main/resources/ng/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component, TemplateRef } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+ 
+  title = 'app';
+  constructor() {}
+
+  
+}
